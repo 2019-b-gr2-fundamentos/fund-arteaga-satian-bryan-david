f231950890f595f0f2cb3cbcfd13e7e1344644e1
ARTEAGA1811 <bryan.arteaga@epn.edu.ec>
2019-10-23 23:34:16 -0500

    act

--- Clases/variables02-ts.js
+++ Clases/variables02-ts.js
@@ -7,5 +7,15 @@ const decimal = 10.12;
 const respuestaTiponumero = typeof numero;
 console.log(respuestaTiponumero);
 console.log(typeof decimal);
+console.log(respuestaTipoNumero);
+console.log(typeof letra)
+console.log(typeof palabra)
+console.log(typeof tipos)
+console.log(typeof casado)
+console.log(typeof fecha)
+console.log(typeof holaa )
+// Gestor de paquetes de node.js
+// npm y yarn
+// comunicarse con el repositorio y descargar el paquete
 //Gestor de paquetes
 //de Node js --> Comunicarse con repositorio 
