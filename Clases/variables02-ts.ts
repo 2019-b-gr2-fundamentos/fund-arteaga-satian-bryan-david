d3fb4a2a5c1b04fa1fbb881f5f3790d41f7deff2
ARTEAGA1811 <bryan.arteaga@epn.edu.ec>
2019-10-23 22:57:56 -0500

    Agregar carpeta clases

---
+++ Clases/variables02-ts.ts
@@ -0,0 +1,19 @@
+//Typescript
+const iva=1;
+//iva=2;    
+
+//Variables Primitivas
+const numero = 10
+const decimal = 10.12
+const respuestaTiponumero = typeof numero; 
+console.log(respuestaTiponumero)
+console.log(typeof decimal);
+const letra = "a";
+const palabra = "palabra"
+console.log(typeof letra);
+console.log(typeof palabra);
+
+
+//Gestor de paquetes
+//de Node js --> Comunicarse con repositorio 
+
