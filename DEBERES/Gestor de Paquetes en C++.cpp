81506ffc06607162407b49edc95dcdf4dcdb3ef7
ARTEAGA1811 <bryan.arteaga@epn.edu.ec>
2019-10-30 01:10:51 -0500

    Consulta gestor de paquetes en C++

---
+++ DEBERES/Gestor de Paquetes en C++.cpp
@@ -0,0 +1,12 @@
+//Deber: ¿Qué gestores de paquetes hay en C++?
+/**
+Lastimosamente no existe un gestor de paquetes en C/C++. Al menos no de manera universal.
+
+Lo que sí existen son  algunas soluciones en esa línea, pero son dependientes del IDE o
+ de un framework en particular.
+
+ Webgrafía:
+ https://es.stackoverflow.com/questions/216682/tenemos-en-c-o-c-alg%C3%BAn-gestor-de-paquetes
+
+ **/
+
