6c74c7308f2cce1f830e6dfbbeba85fabf9deb65
ARTEAGA1811 <bryan.arteaga@epn.edu.ec>
2019-10-31 17:15:41 -0500

    Deberes

--- DEBERES/Calculadora.js
+++ DEBERES/Calculadora.js
@@ -1,36 +1,37 @@
 console.log("\tBienvenido, este programa simula una calculadora\n\nElija su oción preferida:");
 console.log("Suma: 1.\nResta: 2.\nMultiflicación: 3.\nDivisión: 4.\n");
-var ElegirOpcion = prompt("Choose now or die: ");
-if (ElegirOpcion == 1 || ElegirOpcion == 2 || ElegirOpcion == 3 || ElegirOpcion == 4) {
-    var ValorUnoString = prompt("Ingresa el primer valor: ");
-    var ValorDosString = prompt("Ingresa el segundo valor: ");
-    var NumUno = Number(ValorUnoString);
-    var NumDos = Number(ValorDosString);
-    if (ElegirOpcion == 1) {
+var ElegirOpcionString = prompt("Choose now: ");
+var ElegirOpcion = Number(ElegirOpcionString);
+var ValorUnoString = prompt("Ingresa el primer valor: ");
+var ValorDosString = prompt("Ingresa el segundo valor: ");
+var NumUno = Number(ValorUnoString);
+var NumDos = Number(ValorDosString);
+switch (ElegirOpcion) {
+    case 1:
         console.log("Has elegido la Suma: ");
         console.log(NumUno);
         console.log(" + ");
         console.log(NumDos);
         console.log("El resultado es: ");
         console.log(NumUno + NumDos);
-    }
-    if (ElegirOpcion == 2) {
+        break;
+    case 2:
         console.log("Has elegido la Resta: ");
         console.log(NumUno);
         console.log(" - ");
         console.log(NumDos);
         console.log("El resultado es: ");
         console.log(NumUno - NumDos);
-    }
-    if (ElegirOpcion == 3) {
+        break;
+    case 3:
         console.log("Has elegido la Multiplicación: ");
         console.log(NumUno);
         console.log(" * ");
         console.log(NumDos);
         console.log("El resultado es: ");
         console.log(NumUno * NumDos);
-    }
-    if (ElegirOpcion == 4) {
+        break;
+    case 4:
         console.log("Has elegido la División: ");
         if (NumDos == 0) {
             console.log("No se puede dividir para 0.");
@@ -42,8 +43,8 @@ if (ElegirOpcion == 1 || ElegirOpcion == 2 || ElegirOpcion == 3 || ElegirOpcion 
             console.log("El resultado es: ");
             console.log(NumUno / NumDos);
         }
-    }
-}
-else {
-    console.log("Has elegido un número que no está en las opciones.");
+        break;
+    default:
+        console.log("Has elegido un número que no está en las opciones.");
+        break;
 }
--- DEBERES/Calculadora.ts
+++ DEBERES/Calculadora.ts
@@ -1,38 +1,48 @@
 console.log("\tBienvenido, este programa simula una calculadora\n\nElija su oción preferida:");
 console.log("Suma: 1.\nResta: 2.\nMultiflicación: 3.\nDivisión: 4.\n");
-const ElegirOpcion = prompt("Choose now: ");
+const ElegirOpcionString = prompt("Choose now: ");
+const ElegirOpcion = Number(ElegirOpcionString);
 
-if(ElegirOpcion==1||ElegirOpcion==2||ElegirOpcion==3||ElegirOpcion==4){
 
 const ValorUnoString = prompt("Ingresa el primer valor: ");
 const ValorDosString = prompt("Ingresa el segundo valor: ");
 const NumUno = Number(ValorUnoString);
 const NumDos = Number(ValorDosString);
 
-    if(ElegirOpcion == 1){
-        console.log("Has elegido la Suma: ");
-        console.log(NumUno); console.log(" + "); console.log(NumDos);
-        console.log("El resultado es: "); console.log(NumUno+NumDos);
-    }
-    if(ElegirOpcion == 2){
-        console.log("Has elegido la Resta: ");
-        console.log(NumUno); console.log(" - "); console.log(NumDos);
-        console.log("El resultado es: "); console.log(NumUno-NumDos);
-    }
-    if(ElegirOpcion == 3){
-        console.log("Has elegido la Multiplicación: ");
-        console.log(NumUno); console.log(" * "); console.log(NumDos);
-        console.log("El resultado es: "); console.log(NumUno*NumDos);
-    }
-    if(ElegirOpcion == 4){
-        console.log("Has elegido la División: ");
+switch(ElegirOpcion){
 
-        if(NumDos == 0){ console.log("No se puede dividir para 0.")}
-        else{
-        console.log(NumUno); console.log(" / "); console.log(NumDos);
-        console.log("El resultado es: "); console.log(NumUno/NumDos); }
-    }
+      
 
- } else{
-    console.log("Has elegido un número que no está en las opciones.");
+    case 1:
+            console.log("Has elegido la Suma: ");
+            console.log(NumUno); console.log(" + "); console.log(NumDos);
+            console.log("El resultado es: "); console.log(NumUno+NumDos);
+            break;
+
+    case 2:
+            console.log("Has elegido la Resta: ");
+            console.log(NumUno); console.log(" - "); console.log(NumDos);
+            console.log("El resultado es: "); console.log(NumUno-NumDos);
+            break;
+
+    case 3:
+            console.log("Has elegido la Multiplicación: ");
+            console.log(NumUno); console.log(" * "); console.log(NumDos);
+            console.log("El resultado es: "); console.log(NumUno*NumDos);
+            break;
+
+    case 4:
+            console.log("Has elegido la División: ");
+
+            if(NumDos == 0){ console.log("No se puede dividir para 0.")}
+            else{
+            console.log(NumUno); console.log(" / "); console.log(NumDos);
+            console.log("El resultado es: "); console.log(NumUno/NumDos); }
+            break;
+
+    default:
+        console.log("Has elegido un número que no está en las opciones.");
+        break;
+
 }
+
