628e791dac698994bdde73ab7be3553146afb158
ARTEAGA1811 <bryan.arteaga@epn.edu.ec>
2019-11-06 15:17:40 -0500

    Revert "06 switch"
    This reverts commit 89bff5434d0234913f946f67136303f7bf21e8c1.

--- DEBERES/Deberes en js y ts/Calculadora.ts
+++ DEBERES/Deberes en js y ts/Calculadora.ts
@@ -1,58 +1,70 @@
 console.log("\tBienvenido, este programa simula una calculadora\n\nElija su oción preferida:");
 console.log("Suma: 1.\nResta: 2.\nMultiflicación: 3.\nDivisión: 4.\nPotencia de un número: 5.");
-const ElegirOpcionString = prompt("Suma: 1.\nResta: 2.\nMultiflicación: 3.\nDivisión: 4.\nPotencia de un número: 5.\nChoose now: ");
+const ElegirOpcionString = prompt("Choose now: ");
 const ElegirOpcion = Number(ElegirOpcionString);
 const union = (ElegirOpcion == 1 || ElegirOpcion==2 || ElegirOpcion==3 || ElegirOpcion==4 || ElegirOpcion==5);
 
 
 if(union){
-    const ValorUnoString = prompt("Ingresa el primer valor: ");
-    const ValorDosString = prompt("Ingresa el segundo valor: ");
-    const ValorUno = Number(ValorUnoString);
-    const ValorDos = Number(ValorDosString);
 
       
 
-    switch(ElegirOpcion){
+        switch(ElegirOpcion){
         
 
-        case 1:
+                case 1:
             console.log("Has elegido la Suma: ");
-            console.log(ValorUno + " + " + ValorDos); 
-            console.log("El resultado es: "); console.log(ValorUno+ValorDos);
+            const ValorUnoSumaString = prompt("Ingresa el primer valor: ");
+                const ValorDosSumaString = prompt("Ingresa el segundo valor: ");
+                const NumUnoSuma = Number(ValorUnoSumaString);
+                const NumDosSuma = Number(ValorDosSumaString);
+            console.log(NumUnoSuma); console.log(" + "); console.log(NumDosSuma);
+            console.log("El resultado es: "); console.log(NumUnoSuma+NumDosSuma);
             break;
-            
-        case 2:
+
+                case 2:
             console.log("Has elegido la Resta: ");
-            console.log(ValorUno + " - " + ValorDos); 
-            console.log("El resultado es: "); console.log(ValorUno-ValorDos);
+            const ValorUnoRestaString = prompt("Ingresa el primer valor: ");
+        const ValorDosRestaString = prompt("Ingresa el segundo valor: ");
+        const NumUnoResta = Number(ValorUnoRestaString);
+        const NumDosResta = Number(ValorDosRestaString);
+            console.log(NumUnoResta); console.log(" - "); console.log(NumDosResta);
+            console.log("El resultado es: "); console.log(NumUnoResta-NumDosResta);
             break;
 
-        case 3:
-            console.log("Has elegido la Multiplicación: ");       
-            console.log(ValorUno + " * " + ValorDos); 
-            console.log("El resultado es: "); console.log(ValorUno*ValorDos);
+                case 3:
+            console.log("Has elegido la Multiplicación: ");
+            const ValorUnoMultiplicacionString = prompt("Ingresa el primer valor: ");
+        const ValorDosMultiplicacionString = prompt("Ingresa el segundo valor: ");
+        const NumUnoMultiplicacion = Number(ValorUnoMultiplicacionString);
+        const NumDosMultiplicacion = Number(ValorDosMultiplicacionString);
+            console.log(NumUnoMultiplicacion); console.log(" * "); console.log(NumDosMultiplicacion);
+            console.log("El resultado es: "); console.log(NumUnoMultiplicacion*NumDosMultiplicacion);
             break;
 
-        case 4:
+                case 4:
             console.log("Has elegido la División: ");
-            
-                //Condicionales en caso que se divida para cero.
-                if(ValorDos == 0){ 
-                    console.log("No se puede dividir para 0.");
-                }else{
-                    console.log(ValorUno); console.log(" / "); console.log(ValorDos);
-                    console.log("El resultado es: "); console.log(ValorUno/ValorDos);
-                }
+            const ValorUnoDivisionString = prompt("Ingresa el primer valor: ");
+            const ValorDosDivisionString = prompt("Ingresa el segundo valor: ");
+            const NumUnoDivision = Number(ValorUnoDivisionString);
+            const NumDosDivision = Number(ValorDosDivisionString);
+
+            if(NumDosDivision == 0){ console.log("No se puede dividir para 0.")}
+            else{
+            console.log(NumUnoDivision); console.log(" / "); console.log(NumDosDivision);
+            console.log("El resultado es: "); console.log(NumUnoDivision/NumDosDivision); }
             break;
 
-        case 5:
-            console.log("Has elegido la potenciacion" );
-            console.log(ValorUno + "^" + ValorDos);
-            console.log("El resultado es: "); console.log(Math.pow(ValorUno,ValorDos));
-            break;
+            case 5:
+                console.log("Has elegido la potenciacion" );
+                const ValorUnoPotenciaString = prompt("Ingresa un numero: ");
+                const elevadoaString = prompt("Para cuanto quieres elevar el numero? ");
+                const ValorUnoPotencia = Number(ValorUnoPotenciaString);
+                const elevadoa = Number(elevadoaString);
+                console.log(ValorUnoPotencia + "^" + elevadoa);
+                console.log("El resultado es: "); console.log(Math.pow(ValorUnoPotencia,elevadoa));
 
-    }
+        }
 
 }else{
         console.log("Has elegido un número que no está en las opciones.");
