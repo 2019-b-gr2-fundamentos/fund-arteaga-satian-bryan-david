ad1354b518fa575a20408d4d1a3e34d09dcdf13f
ARTEAGA1811 <bryan.arteaga@epn.edu.ec>
2019-11-06 20:24:23 -0500

    05-prompt

---
+++ Clases/01-introduccion/05-prompt.js
@@ -0,0 +1,6 @@
+var numeroUnoString = prompt("Ingresa el número uno"); //String
+var numeroDosString = prompt("Ingresa el número dos"); // String
+console.log(numeroUnoString + numeroDosString);
+var numeroUno = number("Ingresa el num 1: ");
+var numeroDos = number("Ingresa el num 2: ");
+console.log(numeroUno + numeroDos);
