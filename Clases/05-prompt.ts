d50d994ae2315975d21d0d45806fed95947519a0
ARTEAGA1811 <bryan.arteaga@epn.edu.ec>
2019-11-06 20:23:04 -0500

    mover carpeta

--- Clases/05-prompt.ts
+++
@@ -1,7 +0,0 @@
-const numeroUnoString = prompt("Ingresa el número uno"); //String
-const numeroDosString = prompt("Ingresa el número dos");  // String
-console.log(numeroUnoString + numeroDosString);
-
-const numeroUno = number("Ingresa el num 1: ");
-const numeroDos = number("Ingresa el num 2: ");
-console.log(numeroUno + numeroDos);
